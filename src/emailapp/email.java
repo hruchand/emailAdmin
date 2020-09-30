755ed0fde077109fe546acbb106e0d5cdbc005a3
Harshvardhan Ruchandani <hruchand@outlook.com>
2020-09-30 10:11:08 -0500

    Final commit

--- src/emailapp/email.java
+++ src/emailapp/email.java
@@ -2,6 +2,7 @@ package emailapp;
 
 import java.util.Scanner;
 
+// email class to handle and manipulate user information
 public class email {
     private String fname;
     private String lname;
@@ -12,27 +13,28 @@ public class email {
     private String altEmail;
     final int defPassLen = 8;
     final String emailSuff = "company.com";
+    final int defMailboxCap = 500;
 
     // constructor to get fname & lname
     public email(String firstName, String lastName) {
         this.fname = firstName;
         this.lname = lastName;
-        System.out.println(firstName + "\t\t" + lastName);
+        //System.out.println(firstName + "\t\t" + lastName);
         this.dept = setDept();
-        System.out.println("Your Dept set to : " + this.dept);
+        //System.out.println("Your Dept set to : " + this.dept);
         this.password = genRandomPass(defPassLen);
-        System.out.println("Default password:" + this.password);
-        this.email = firstName.toLowerCase() + "." + lastName.toLowerCase()+ "@" +this.emailSuff;
-        System.out.println("Your new company email: " + this.email);
+        //System.out.println("Default password:" + this.password);
+        this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + this.emailSuff;
+        //System.out.println("Your new company email: " + this.email);
     }
 
-    private String setDept(){
+    // Set Dept by taking input from the user
+    private String setDept() {
         String ret_string = "\0";
         System.out.println("Enter Choice\n A. Sales\n B. Dev\n C. Accounts\n");
         Scanner in = new Scanner(System.in);
         char deptChosen = in.next().charAt(0);
-        switch (deptChosen)
-        {
+        switch (deptChosen) {
             case 'A':
                 ret_string = "Sales";
                 break;
@@ -46,39 +48,47 @@ public class email {
                 ret_string = "Unknown";
                 break;
         }
-        return  ret_string;
+        return ret_string;
     }
-
-    private String genRandomPass(int len){
+// Random password generation for the account
+    private String genRandomPass(int len) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
         char[] pass = new char[len];
-        for (int i =0; i<len;i++)
-        {
-            int random = (int)(Math.random() * passwordSet.length());
+        for (int i = 0; i < len; i++) {
+            int random = (int) (Math.random() * passwordSet.length());
             pass[i] = passwordSet.charAt(random);
         }
         return new String(pass);
     }
 
-public void setMailboxCap(int cap){
+    // setter for mailbox cap
+    public void setMailboxCap(int cap) {
         this.mailboxCap = cap;
-}
-
-public void setAltEmail(String altemail){
+    }
+    // setter for alternate email
+    public void setAltEmail(String altemail) {
         this.altEmail = altemail;
-}
-
-public void changePass(String pass){
+    }
+// setter for chnage password
+    public void changePass(String pass) {
         this.password = password;
-}
-
-public int getMailboxCap(){
+    }
+//getter for mailbox cap
+    public int getMailboxCap() {
         return this.mailboxCap;
-}
-public String getAltEmail(){
+    }
+// getter for alt email
+    public String getAltEmail() {
         return this.altEmail;
-}
-public String getPassword(){
+    }
+// getter for get password
+    public String getPassword() {
         return this.password;
-}
+    }
+// get info function to display account info to the user
+    public String getInfo() {
+        return "\nFirstname = " + fname + "\nLastname = " +
+                lname + "\nEmail = " + email + "\nAlt email = " + altEmail +
+                "\nPassword = " + password + "\nMailbox cap = " + mailboxCap;
+    }
 }
--- src/emailapp/emailApp.java
+++ src/emailapp/emailApp.java
@@ -1,9 +1,13 @@
 package emailapp;
 
+// Main class of the project
 public class emailApp {
     public static void main(String[] args) {
         email email1 = new email("Harsh", "Ruch");
-
+        email1.setAltEmail("xx.yy@gmail.com");
+        email1.setMailboxCap(email1.defMailboxCap);
+        //System.out.println("Alt Email is "+ email1.getAltEmail());
+        System.out.println(email1.getInfo());
 
     }
 }
